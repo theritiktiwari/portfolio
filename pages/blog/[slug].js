9b3ad2f0b88f3a8fa0ad031c9339c35b0e779de3
Ritik Tiwari <ritiktiwariwork@gmail.com>
2022-10-15 02:39:04 +0530

    Add Blog section

---
+++ pages/blog/[slug].js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Link from 'next/link'
+import PortableText from "react-portable-text";
+
+const Slug = ({ client, imgURL, name, router }) => {
+    const dummyImage = "https://i.ibb.co/8xRxbNh/Blog.png";
+
+    const slug = router.query.slug;
+    const [post, setPost] = useState();
+    const [allPost, setAllPost] = useState();
+    const [nameCat, setNameCat] = useState()
+
+    useEffect(() => {
+        setPost();
+        setAllPost();
+        setNameCat();
+        const getData = async () => {
+            const data1 = await client.fetch(`*[_type == "post" && slug.current == "${slug}"]`);
+            const data2 = await client.fetch(`*[_type == "post" && slug.current != "${slug}"][0...2] | order(publishedAt desc)`);
+            const data3 = await client.fetch(`*[_type == "post" && slug.current == "${slug}"][0]{title, "author": author->name, "categories": categories[]->title}`);
+            setPost(data1[0]);
+            setAllPost(data2);
+            setNameCat(data3);
+        }
+        getData();
+    }, [router]);
+
+    const date = (date) => {
+        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const dateObj = new Date(date);
+        return `${month[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`;
+    }
+
+    return (
+        <>
+            <Head>
+                <title>{post && post.title} | {name}</title>
+            </Head>
+
+            {(post) ? <div className="blog-post">
+                <div className="container">
+                    <div className='box'>
+                        <div className='image' data-aos="zoom-in">
+                            <img src={post.mainImage ? imgURL(post.mainImage).url() || dummyImage : dummyImage} alt={post.title} style={{ filter: "brightness(95%)" }} />
+                        </div>
+                        <div className="details">
+                            <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
+                                {post.publishedAt ? <div>
+                                    <p>Date : <span className='text-uppercase text-bold'>{date(post.publishedAt)}</span></p>
+                                </div> : null}
+                                {nameCat.author ? <div>
+                                    <p>By :&nbsp;<span className='text-uppercase text-bold'>{nameCat.author}</span></p>
+                                </div> : null}
+                            </div>
+                            {nameCat.categories ? <div>
+                                <p>Categories :&nbsp;
+                                    {nameCat.categories.map((cat, catIndex) => {
+                                        return <span key={catIndex} className='text-uppercase text-bold'>{cat}{(nameCat.categories.length != catIndex + 1) && ", "}</span>
+                                    })}
+                                </p>
+                            </div> : null}
+                        </div>
+
+                        <h1 className='heading'>{post.title}</h1>
+                        <div className="blog-content">
+                            {post.body && <PortableText className='text-justify'
+                                content={post.body}
+                                serializers={{
+                                    h1: (props) => <h1 style={{ color: "red" }} {...props} />,
+                                    li: ({ children }) => <li className="special-list-item">{children}</li>
+                                }}
+                            />}
+                        </div>
+                    </div>
+
+                    <hr style={{ margin: "40px" }} />
+
+
+                    <h3 className='text-bold'>Related Posts</h3>
+                    <div className="related-post" data-aos="zoom-in">
+                        {allPost.map((item, index) => {
+                            return <div key={index}>
+                                <Link href={`/blog/${item.slug.current}`}><a>
+                                    <div className="card">
+                                        <div className="image">
+                                            <img src={imgURL(item.mainImage).width(1800).height(1200).url() || dummyImage} width="1800" height="1200" alt={item.slug.current} style={{ filter: "brightness(95%)" }} />
+                                        </div>
+                                        <div className="text-justify">
+                                            <h3>{item.title.substr(0, 17)}...</h3>
+                                            <p>{item.metaDescription.substr(0, 135)}...</p>
+                                        </div>
+                                    </div>
+                                </a></Link>
+                            </div>
+                        })}
+
+                    </div>
+                </div>
+            </div> : null}
+        </>
+    )
+}
+
+export default Slug;
---
+++ pages/blog/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+
+const Index = ({ name, client, router, imgURL }) => {
+    const dummyImage = "https://i.ibb.co/8xRxbNh/Blog.png";
+
+    const [post, setPost] = useState();
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await client.fetch(`*[_type == "post"] | order(publishedAt desc)`);
+            setPost(data);
+        }
+        getData();
+    }, [router]);
+
+    const date = (date) => {
+        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const dateObj = new Date(date);
+        return `${month[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`;
+    }
+
+    const getCategories = (category, index) => {
+        category.map((item, ind) => {
+            client.fetch(`*[_type == "category" && _id == "${item._ref}"]{title}`).then(data => {
+                document.querySelector(`#category${index}`).innerHTML = `${data[0].title}${category.length > 1 ? '...' : ''}`;
+            }).catch(err => {
+                // console.log(err);
+            })
+        })
+    }
+
+    const getAuthors = (author, index) => {
+        client.fetch(`*[_type == "author" && _id == "${author._ref}"]{name}`).then(data => {
+            document.querySelector(`#author${index}`).innerHTML = `${data[0].name}`;
+        }).catch(err => {
+            // console.log(err);
+        })
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Blog | {name}</title>
+            </Head>
+
+            <section className='blog'>
+                <h1>Blogs</h1>
+
+                <div className="container">
+                    {(post && post.length > 0) ? post.map((item, index) => {
+                        return <div className="card" key={index}>
+                            <div className="image">
+                                <img src={item.mainImage ? imgURL(item.mainImage).url() || dummyImage : dummyImage} alt={item.slug.current} />
+                            </div>
+                            <div className="content">
+                                <h3 className="card-title">{(item.title).toLowerCase()}</h3>
+                                <div>
+                                    {item.metaDescription && <p style={{ textAlign: "justify" }}>{item.metaDescription.substr(0, 190)}...</p>}
+
+                                    {item.author ? <p>By : <span id={`author${index}`} className='text-uppercase text-bold'> {getAuthors(item.author, index)}</span></p> : null}
+                                    {item.categories ? <p>Categories : <span id={`category${index}`} className='text-uppercase text-bold'> {getCategories(item.categories, index)}</span></p> : null}
+
+                                    {item.publishedAt ? <p>Date : <span className='text-bold text-uppercase'>{date(item.publishedAt)}</span></p> : null}
+                                </div>
+                                <Link href={`/blog/${item.slug.current}`}><a className='btn'>Read More</a></Link>
+                            </div>
+                        </div>
+                    }) : null}
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Index;
