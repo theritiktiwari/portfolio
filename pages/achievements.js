28094c8f5e56131e10181abaeeac93c13c3eacb3
Ritik Tiwari <ritiktiwariwork@gmail.com>
2023-03-19 21:39:12 +0530

    Some changes

--- pages/achievements.js
+++ pages/achievements.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-
 import ModalCard from '../Components/ModalCard';
-import Mode from '../Components/Mode';
 
 const Achievements = ({ name, client, router, imgURL }) => {
     const [achievementData, setAchievementData] = useState();
@@ -19,23 +17,20 @@ const Achievements = ({ name, client, router, imgURL }) => {
             <Head>
                 <title>Achievements | {name}</title>
             </Head>
-            <Mode work={true} name={name} />
             <section className='info-container'>
-                {
-                    achievementData && achievementData.map((val, index) => {
-                        return <ModalCard
-                            imgURL={imgURL}
-                            key={index}
-                            info={true}
-                            img={val.image}
-                            date={val.date}
-                            title={val.title}
-                            position={val.position}
-                            link={val.link}
-                            description={val.description}
-                        />
-                    })
-                }
+                {achievementData && achievementData.map((val, index) => {
+                    return <ModalCard
+                        imgURL={imgURL}
+                        key={index}
+                        info={true}
+                        img={val.image}
+                        date={val.date}
+                        title={val.title}
+                        position={val.position}
+                        link={val.link}
+                        description={val.description}
+                    />
+                })}
             </section>
         </>
     )
